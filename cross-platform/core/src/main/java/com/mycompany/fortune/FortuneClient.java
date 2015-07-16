10a21c908c1d41894463987dc4d9ae5d32ca4041
Vladimir Pasquier <vpasquier@nuxeo.com>
2015-07-16 14:57:16 +0200

    make robovm work with automation-android

--- cross-platform/core/src/main/java/com/mycompany/fortune/FortuneClient.java
+++ cross-platform/core/src/main/java/com/mycompany/fortune/FortuneClient.java
@@ -1,20 +1,17 @@
 package com.mycompany.fortune;
 
-import org.nuxeo.ecm.automation.client.Session;
+import org.nuxeo.ecm.automation.client.jaxrs.Session;
 import org.nuxeo.ecm.automation.client.jaxrs.impl.HttpAutomationClient;
-import org.nuxeo.ecm.automation.client.model.Document;
+import org.nuxeo.ecm.automation.client.jaxrs.model.Document;
 import retrofit.Callback;
 import retrofit.RestAdapter;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 import retrofit.http.GET;
 
-import java.io.IOException;
 
 public class FortuneClient {
     private static final String API_URL = "http://www.iheartquotes.com/api/v1/";
-
-    public Document root;
 
     private class Fortune {
         public String quote;
@@ -37,18 +34,6 @@ public class FortuneClient {
                 .setEndpoint(API_URL)
                 .build();
         service = adapter.create(FortuneService.class);
-        HttpAutomationClient client = new HttpAutomationClient
-                ("https://nightly.nuxeo.com/nuxeo/site/automation", 60000);
-
-        Session session = null;
-        try {
-            session = client.getSession();
-            root = (Document) session.newRequest("Repository.GetDocument")
-                    .set("value", "/").execute();
-            System.out.println(root.getTitle());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public void getFortune(final OnFortuneListener listener) {
@@ -60,7 +45,16 @@ public class FortuneClient {
 
             @Override
             public void failure(RetrofitError error) {
-                listener.onFortune(error.toString());
+                HttpAutomationClient client = new HttpAutomationClient("https://nightly.nuxeo.com/nuxeo/site/automation");
+                Session session = client.getSession("Administrator", "Administrator");
+                Document root = null;
+                try {
+                    root = (Document) session.newRequest("Repository" +
+                            ".GetDocument").set("value", "/").execute();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                listener.onFortune(root.getTitle());
             }
         });
     }
